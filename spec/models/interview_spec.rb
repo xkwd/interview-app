3f01b1ee22d28ca659651503c88ff4ce2d2779ba
xkwd <34714644+xkwd@users.noreply.github.com>
2017-12-31 17:38:47 +0100

    Add through test for Interview model

--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -21,6 +21,6 @@ RSpec.describe Interview, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
   it { should have_many(:answers) }
-  it { should have_many(:sections) }
+  it { should have_many(:sections).through(:answers) }
   it { should belong_to(:user) }
 end
