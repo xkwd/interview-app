bc32b1552603df39db4b5e3c31a05bee732c542d
xkwd <34714644+xkwd@users.noreply.github.com>
2018-01-02 10:22:42 +0100

    Generate Interviews controller

---
+++ app/controllers/interviews_controller.rb
@@ -0,0 +1,7 @@
+class InterviewsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'interviews/index'
+
+  get 'interviews/show'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
