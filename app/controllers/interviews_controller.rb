8d508cfb6131ba4d4cef4f246196eba520596ec1
xkwd <34714644+xkwd@users.noreply.github.com>
2018-01-03 15:09:51 +0100

    Modify index, show Interview views, fix Faker lorem stringification

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,7 +1,10 @@
 class InterviewsController < ApplicationController
   def index
+    @interviews = Interview.all.order("created_at DESC")
   end
 
   def show
+    @interview = Interview.find(params[:id])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'interviews/index'
-
-  get 'interviews/show'
-
+  resources :interviews, only: [:index, :show]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
