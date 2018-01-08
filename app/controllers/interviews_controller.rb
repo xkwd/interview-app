cf52fafa96b75e42a7a5f154beaa44f976b39f25
xkwd <34714644+xkwd@users.noreply.github.com>
2018-01-08 15:19:43 +0100

    Add link allowing user to edit own interviews

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,4 +1,6 @@
 class InterviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:edit]
+
   def index
     @interviews = Interview.all.order("created_at DESC")
   end
@@ -9,6 +11,10 @@ class InterviewsController < ApplicationController
 
   def edit
     @interview = Interview.find(params[:id])
+    unless @interview.user_id == current_user.id
+      flash[:notice] = 'Access denied as you are not owner of this Interview'
+      redirect_to interview_path
+     end
   end
 
   def user_interview
