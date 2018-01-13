021695bdec2f4e050fd57dd90eb2596fa76a9cf1
xkwd <34714644+xkwd@users.noreply.github.com>
2018-01-13 23:45:48 +0100

    Add index shoulda specs and remove the only view spec

--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -21,6 +21,8 @@
 require 'rails_helper'
 
 RSpec.describe Interview, type: :model do
+  it { should have_db_index(:user_id) }
+  it { should have_db_index(:country_id) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
   it { should have_many(:answers) }
