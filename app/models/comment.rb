5c2662c646deb31d1615005421ce6590424448be
xkwd <34714644+xkwd@users.noreply.github.com>
2018-01-25 23:17:20 +0100

    Add conditional validations to Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -21,4 +21,7 @@ class Comment < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable
+  validates :body, presence: true
+  validates :commenter_name, :commenter_email, presence: true, unless: :user_id
+  validates :user_id, presence: true, unless: :commenter_name
 end
