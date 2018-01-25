df521674521b8cfc0b2eefee9d7dbc70be4faf0e
xkwd <34714644+xkwd@users.noreply.github.com>
2018-01-25 23:20:02 +0100

    Create Comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -20,5 +20,24 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:interview) { FactoryBot.create(:interview) }
+  let(:anonymous_comment) { FactoryBot.create(:comment, user_id: nil) }
+  let(:user_comment) { FactoryBot.create(:comment, commenter_name: nil, commenter_email: nil) }
+
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:commenter_name) }
+  it { should validate_presence_of(:commenter_email) }
+  it { should validate_presence_of(:user_id) }
+  it { should belong_to(:commentable) }
+  it { should belong_to(:user) }
+  it { should have_many(:comments) }
+  it { should have_db_index(:user_id) }
+
+  it "is valid for guest user" do
+    expect(anonymous_comment).to be_valid
+  end
+
+  it "is valid for registered user" do
+    expect(user_comment).to be_valid
+  end
 end
