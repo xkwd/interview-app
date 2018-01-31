eeb7381d8cee8666c3d6ed4f7c58119ca5274d0a
xkwd <34714644+xkwd@users.noreply.github.com>
2018-01-31 22:30:01 +0100

    Fix styling and set before_action

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,5 +1,5 @@
 class InterviewsController < ApplicationController
-  before_action :authenticate_user!, only: [:edit]
+  before_action :authenticate_user!, only: [:edit, :user_interview]
 
   def index
     @interviews = Interview.published.order("created_at DESC")
@@ -11,7 +11,7 @@ class InterviewsController < ApplicationController
 
   def new
     @interview = current_user.interviews.build
-    @countries = Country.all.map{|c| [c.name, c.id]}
+    @countries = Country.all.map { |c| [c.name, c.id] }
     Section.all.each do |section|
       @interview.answers.build(
         content: "",
@@ -32,7 +32,7 @@ class InterviewsController < ApplicationController
 
   def edit
     @interview = Interview.find(params[:id])
-    @countries = Country.all.map{|c| [c.name, c.id]}
+    @countries = Country.all.map { |c| [c.name, c.id] }
     unless @interview.user_id == current_user.id
       flash[:notice] = 'Access denied as you are not owner of this Interview'
       redirect_to interview_path
