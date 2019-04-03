c8bc5766a8250aaa160b74550f45f18a617ac683
xkwd <34714644+xkwd@users.noreply.github.com>
2019-04-03 21:55:01 +0200

    Replace static FactoryBot attributes with dynamic ones

--- spec/factories/interview.rb
+++ spec/factories/interview.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :interview do
     title { Faker::Books::Dune.saying }
     description { Faker::Lorem.paragraphs(rand(2...3)).join }
-    published true
+    published { true }
 
     user
     country
