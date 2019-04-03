c2070dcd54bb4d64583f5e35bfb288f67339511e
xkwd <34714644+xkwd@users.noreply.github.com>
2019-04-03 21:46:40 +0200

    Fix Faker deprecation warning

--- spec/factories/interview.rb
+++ spec/factories/interview.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :interview do
-    title { Faker::Dune.saying }
+    title { Faker::Books::Dune.saying }
     description { Faker::Lorem.paragraphs(rand(2...3)).join }
     published true
 
